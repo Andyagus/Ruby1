4fec2e173dc5c160ef3c804d9a7fa42e91ac3e99
Andy Agus <Redagus@gmail.com>
2015-06-22 17:09:01 -0400

    initial commit

---
+++ basicruby.rb
@@ -0,0 +1,47 @@
+# Exercise 1
+
+def add_america(input)
+	puts input.to_s + " only in America!"
+end
+
+a = "Fried Hot Dogs"
+
+# Exercise 2
+
+def max_number(*input)
+	input.max
+end
+
+mymax = max_number(400,4212,10123,12381238)
+# Exercise 3
+
+def combine(arr1,arr2)
+	arr1.
+end
+
+arr1 = ["car", "boat","plane","train"]
+arr2 = ["toyota", "master craft", "boeing","amtrak"]
+
+def combine_array
+  new_hash = {}
+  for i in 0..a1.length
+      my_key = a1[i]
+      new_hash[my_key] = a2[i]
+  end
+end
+
+while 
+  i = 0
+  i < 100 
+  i + 1
+  puts i 
+  i + 1
+end
+
+
+
+
+
+
+
+
