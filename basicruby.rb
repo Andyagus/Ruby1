30a5dbb946c1d66a9dd6480a80882c8140f9eceb
Andy Agus <Redagus@gmail.com>
2015-06-22 22:48:37 -0400

    Working on fizzbuzz statement

--- basicruby.rb
+++ basicruby.rb
@@ -1,45 +1,61 @@
-# Exercise 1
+# # Exercise 1
 
-def add_america(input)
-	puts input.to_s + " only in America!"
-end
+# def add_america(input)
+# 	puts input.to_s + " only in America!"
+# end
 
-a = "Fried Hot Dogs"
+# a = "Fried Hot Dogs"
 
-# Exercise 2
+# # Exercise 2
 
-def max_number(*input)
-	input.max
-end
+# def max_number(*input)
+# 	input.max
+# end
 
-mymax = max_number(400,4212,10123,12381238)
-# Exercise 3
+# mymax = max_number(400,4212,10123,12381238)
+# # Exercise 3
 
-def combine(arr1,arr2)
-	arr1.
-end
+# def combine(arr1,arr2)
+# 	arr1.
+# end
 
-arr1 = ["car", "boat","plane","train"]
-arr2 = ["toyota", "master craft", "boeing","amtrak"]
+# arr1 = ["car", "boat","plane","train"]
+# arr2 = ["toyota", "master craft", "boeing","amtrak"]
 
-def combine_array
-  new_hash = {}
-  for i in 0..a1.length
-      my_key = a1[i]
-      new_hash[my_key] = a2[i]
+# def combine_array
+#   new_hash = {}
+#   for i in 0..a1.length
+#       my_key = a1[i]
+#       new_hash[my_key] = a2[i]
+#   end
+# end
+
+#Exercise fizzbuzz
+
+def fizzbuzz()
+
+  for x in 1..100
+    
+      if (x % 3) == 0
+        puts "fizz"
+      
+
+      else (x % 5) == 0
+        puts "buzz"
+      
+
+      else num1 && num2 == 0 
+        puts "fizzbuzz"
+      
+
+      elsif !((x % 3) && (x % 5) == 0)
+        puts x
+
+    end
+
   end
+
 end
-
-while 
-  i = 0
-  i < 100 
-  i + 1
-  puts i 
-  i + 1
-end
-
-
-
 
 
 
